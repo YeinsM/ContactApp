e0f2b5e98fbc263a60cd47a85322c3d40a896372
YeinsM <wildermancera1@gmail.com>
2022-04-28 14:59:01 -0400

    Add model and data files for contact entity

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ContactBackEnd.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactBackEnd.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Give us set reference for non nullable DbContext .NET 6 case
+        public DbSet<Contact> Contacts => Set<Contact>();
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
