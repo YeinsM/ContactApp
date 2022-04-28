c34ed0ea2f40f5635cdeb94fc57146a286f1b2cc
YeinsM <wildermancera1@gmail.com>
2022-04-28 15:44:35 -0400

    Add functional Model, Controller and Data for Contacts

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,53 @@
+﻿using ContactBackEnd.Data;
+using ContactBackEnd.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ContactBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        // Add a readonly for DbContext Entities
+        private readonly ApplicationDbContext _context;
+        public ContactsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ContactsController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> GetAll()
+        {
+            return await _context.Contacts.ToListAsync();
+        }
+
+        // GET api/<ContactsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ContactsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ContactsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ContactsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Data/Migrations/20220428190955_AddCompleteContact.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactBackEnd.Data.Migrations
+{
+    public partial class AddCompleteContact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Contacts");
+        }
+    }
+}
